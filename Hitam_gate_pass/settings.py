e8b86110bd91e1b50e1810208dd1ca45a910e8cb
MDARIF2005 <arifsmart2005@gmail.com>
2025-04-12 13:56:02 +0530

    first commit

--- Hitam_gate_pass/settings.py
+++ Hitam_gate_pass/settings.py
@@ -80,6 +80,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'MD_Arif',
+        'PASSWORD': 'HITAM@123',
+        'HOST': 'localhost',
+        'PORT': '3307',
     }
 }
 
